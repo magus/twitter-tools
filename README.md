4616e3da448fd04e1bdb1d86833b28d461f22d49
noah <noah@iamnoah.com>
2017-07-12 11:35:30 -0700

    twitter api secrets

---
+++ README.md
@@ -0,0 +1,7 @@
+# twitter-following
+manage following
+
+# Setup
+- `cp config/secrets.json.example config/secrets.json`
+- Add secrets for [Twitter application](https://apps.twitter.com) to `config/secrets.json`
+- `yarn dev`
--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
 import Twit from 'twit';
 
-console.log(Twit);
+// Read in secrets from config/secrets.json
+const SECRETS = require('../config/secrets.json');
+
+const T = new Twit({
+  consumer_key:         SECRETS.consumer_key,
+  consumer_secret:      SECRETS.consumer_secret,
+  access_token:         SECRETS.access_token,
+  access_token_secret:  SECRETS.access_token_secret,
+
+  // optional HTTP request timeout to apply to all requests.
+  timeout_ms:           60*1000,
+});
+
+console.log(T);
