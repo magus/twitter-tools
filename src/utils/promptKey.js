4cb9a3d349395752ac4f02cdac663f7c28579ee7
noah <noah@iamnoah.com>
2017-07-13 20:28:41 -0700

    promptKey utility

---
+++ src/utils/promptKey.js
@@ -0,0 +1,50 @@
+// @flow
+import Output from 'utils/Output';
+
+type VerifyStringFunc = (key: string) => boolean;
+
+export default function promptKey(isValidKey: VerifyStringFunc) {
+  const stdin = process.stdin;
+
+  // without this, we would only get streams once enter is pressed
+  // $FlowFixMe
+  stdin.setRawMode(true);
+
+  // resume stdin in the parent process (node app won't quit all by itself
+  // unless an error or process.exit() happens)
+  stdin.resume();
+
+  // i don't want binary, do you?
+  stdin.setEncoding( 'utf8' );
+
+  return new Promise(resolve => {
+    const handleResolve = key => {
+      // cleanup listener
+      stdin.removeListener('data', onData);
+      // put stdin back how we found it
+      // $FlowFixMe
+      stdin.setRawMode(false);
+      stdin.pause();
+
+      // call onKey handler
+      resolve(key);
+    };
+
+    const onData = key => {
+      // ctrl+c exits prompt
+      if (key === '\u0003') return handleResolve(null);
+
+      // prompt until valid key
+      if (!isValidKey(key)) {
+        Output.warn('invalid input');
+        return;
+      }
+
+      // resolve with valid key
+      handleResolve(key);
+    };
+
+    // on any data into stdin
+    stdin.on('data', onData);
+  });
+}
