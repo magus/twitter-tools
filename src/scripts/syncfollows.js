4ac3a19697344364e59513cb9641d68e26a48925
noah <noah@iamnoah.com>
2017-07-19 04:07:15 -0700

    syncfollows simple

--- src/scripts/syncfollows.js
+++ src/scripts/syncfollows.js
@@ -11,16 +11,6 @@ import friends from 'api/friends';
 import promptKey from 'utils/promptKey';
 import Output from 'utils/Output';
 
-function promptUnfollow(user) {
-  return promptKey(`🤔  ${user.out()} [y/n]`, key => /^y|n$/i.test(key)).then(key => {
-    if (key !== 'y') return;
-
-    return user.unfollow().then(() => {
-      Output.error(`Unfollowed ${user.out()}`);
-    });
-  });
-}
-
 //////////////////////////////////////////////////
 //                      MAIN                    //
 //////////////////////////////////////////////////
@@ -29,9 +19,32 @@ function promptUnfollow(user) {
 friends().then(() => {
   // Select list to sync
   // https://dev.twitter.com/rest/reference/get/lists/list
-  return Cache.get('lists/list').then(data => {
-    Output.info('data', data);
+  return Cache.get('lists/list').then(lists => {
+    Output.out();
+    lists.forEach((list, i) => {
+      Output.info(i, '\t', list.name);
+    });
+    Output.out();
 
-    return data;
+    return lists;
+  })
+}).then(lists => {
+  return promptKey(`🤔  which list? `, key => !!lists[key]).then(key => {
+    const selectedList = lists[key];
+    Output.debug('selectedList.name', selectedList.name);
+    return selectedList;
   });
+}).then(list => {
+  Output.info('State.following', State.following.length);
+
+  // Get current members of list
+  // Iterate following and generate list of members we need to add
+  // Output `Adding X users to list...`
+  Output.start(`Syncing ${'N'} friends to [LIST]${list.name}`);
+  // Chunks into 100 user chunks
+
+  // // Create list membership for missing users
+  // // https://dev.twitter.com/rest/reference/post/lists/members/create_all
+  // const user_id = [].join(',');
+  // Cache.post('lists/members/create_all', { list_id: list.id, user_id });
 });
