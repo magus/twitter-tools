0a18362f147fb7f0420cc973ef65972351d13949
noah <noah@iamnoah.com>
2017-10-05 19:18:48 -0700

    State: +FollowsList

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,6 @@
+// @flow
 type UserMap = { [id: number]: any };
-type State = {
+type State = {|
   // ordered array of user ids (ordered by follow date)
   following: Array<number>,
 
@@ -8,12 +9,17 @@ type State = {
 
   // hash for user id to user entities
   followers: UserMap,
-};
+
+  // hash for user id to user entities
+  // based on Follows list
+  FollowsList: UserMap,
+|};
 
 const STATE: State = {
   following: [],
   users: {},
   followers: {},
+  FollowsList: {},
 };
 
 export default STATE;
